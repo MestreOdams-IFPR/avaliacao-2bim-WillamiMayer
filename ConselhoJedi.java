e050e47afc751ebec7ee3cc002ec7be08368da26
OdairMoreira <odair.moreira.souza>
2025-07-17 15:36:41 -0300

    Conceito D+ | Prof. Odair

--- ConselhoJedi.java
+++ ConselhoJedi.java
@@ -1,17 +1,18 @@
 import java.util.Scanner;
 
 public class ConselhoJedi {
-    //quase consegui essa
+    // quase consegui essa
 
     static Scanner teclado = new Scanner(System.in);
+
     public static void main(String[] args) {
-        
-        int[] vetor = {1, 5, 2, 4, 3};
+
+        int[] vetor = { 1, 5, 2, 4, 3 };
         System.out.println(verPontuacao(vetor));
     }
 
-    public static int verPontuacao(int[] vetor){
-        
+    public static int verPontuacao(int[] vetor) {
+
         int inicio = vetor[0];
         int n = vetor.length - 1;
         int k = 2;
@@ -20,53 +21,54 @@ public class ConselhoJedi {
         int meio = (inicio + n) / 2;
 
         merge(vetor, tempv, inicio, meio, n);
-        
+
         for (int i = 0; i < vetor.length; i++) {
-            
-            if(k < vetor[i]){
+
+            if (k < vetor[i]) {
                 temp += vetor[i];
             }
 
-            if(n <= i){
+            if (n <= i) {
                 break;
             }
         }
         return temp;
     }
 
-    public static void mergeSort(int[] array, int[] temp, int inicio, int fim){
-        if(inicio < fim){
+    public static void mergeSort(int[] array, int[] temp, int inicio, int fim) {
+        if (inicio < fim) {
             int meio = (inicio + fim) / 2;
             mergeSort(array, temp, inicio, meio);
             mergeSort(array, temp, meio + 1, fim);
-            merge(array,temp,inicio,meio,fim);
+            merge(array, temp, inicio, meio, fim);
         }
     }
 
-    public static void sort(int[]array){
-        if(array == null || array.length <2) return;
+    public static void sort(int[] array) {
+        if (array == null || array.length < 2)
+            return;
         int temp[] = new int[array.length];
-        mergeSort(array, temp, 0, array.length -1);
+        mergeSort(array, temp, 0, array.length - 1);
     }
 
-    public static void merge (int[] array, int[] temp, int inicio, int meio, int fim){
+    public static void merge(int[] array, int[] temp, int inicio, int meio, int fim) {
         for (int i = inicio; i <= fim; i++) {
             temp[i] = array[i];
         }
 
         int i = inicio;
-        int j = meio+1;
+        int j = meio + 1;
         int k = inicio;
 
-        while(i <=meio && j <= fim){
-            if(temp[i] <= temp[j]){
+        while (i <= meio && j <= fim) {
+            if (temp[i] <= temp[j]) {
                 array[k++] = temp[i++];
-            } else{
+            } else {
                 array[k++] = temp[j++];
             }
         }
 
-        while(i <=meio){
+        while (i <= meio) {
             array[k++] = temp[i++];
         }
     }
--- Prova.java
+++ Prova.java
@@ -1,20 +1,20 @@
-public class Prova{
+public class Prova {
     public static void main(String[] args) {
         tribonacci(5);
     }
 
-    public static int tribonacci(int n){
+    public static int tribonacci(int n) {
         int num = 0;
         int[] vetor = new int[n + 1];
 
         for (int i = 0; i < vetor.length; i++) {
-            if(n <= 1){
+            if (n <= 1) {
                 return 0;
-            } else if(n == 2){
+            } else if (n == 2) {
                 return 1;
-            } else{
-                
-                num = tribonacci((vetor[n - 1]) + (vetor[n - 2]) + (vetor[n - 3]) );
+            } else {
+
+                num = tribonacci((vetor[n - 1]) + (vetor[n - 2]) + (vetor[n - 3]));
                 System.out.println(num);
                 n--;
             }
@@ -22,19 +22,18 @@ public class Prova{
         return num;
     }
 
-    public static int pell(int[] n){
+    public static int pell(int[] n) {
         int num = 0;
-        
-        int fim = n.length -1;
+
+        int fim = n.length - 1;
 
         for (int i = 0; i < n.length; i++) {
-            if(n[fim] < 1){
+            if (n[fim] < 1) {
                 return 0;
-            } else if(n[fim] == 1){
+            } else if (n[fim] == 1) {
                 return 1;
-            } else{
-                
-                
+            } else {
+
                 System.out.println(num);
                 fim--;
             }
@@ -42,6 +41,7 @@ public class Prova{
         return num;
     }
 
-    //as de string eu só não tive tempo, fiquei um tempo preso nessa
-    //A tribonacci e a pell ficaram incompletas, eu até entendi a lógica da tribonacci só que não soube aplicar, a pell só foi triste
+    // as de string eu só não tive tempo, fiquei um tempo preso nessa
+    // A tribonacci e a pell ficaram incompletas, eu até entendi a lógica da
+    // tribonacci só que não soube aplicar, a pell só foi triste
 }
